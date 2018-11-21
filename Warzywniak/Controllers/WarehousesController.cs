faa01f1c1af3076542b51eefef9e08a9060a5b8a
msciasteczkowypotwor <msciasteckowypotwor@gmail.com>
2018-11-21 21:35:11 +0100

    wq

---
+++ Warzywniak/Controllers/WarehousesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Warzywniak.Models;
+
+namespace Warzywniak.Controllers
+{
+    public class WarehousesController : Controller
+    {
+        private WarzywniakEntities db = new WarzywniakEntities();
+
+        // GET: Warehouses
+        public ActionResult Index()
+        {
+            var warehouses = db.Warehouses.Include(w => w.Order).Include(w => w.Product);
+            return View(warehouses.ToList());
+        }
+
+        // GET: Warehouses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Create
+        public ActionResult Create()
+        {
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "OrderId");
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName");
+            return View();
+        }
+
+        // POST: Warehouses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WarehouseId,ProductId,OrderId,ExpiryDate,Quantity")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Warehouses.Add(warehouse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "OrderId", warehouse.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "OrderId", warehouse.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WarehouseId,ProductId,OrderId,ExpiryDate,Quantity")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(warehouse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "OrderId", warehouse.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", warehouse.ProductId);
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            db.Warehouses.Remove(warehouse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Warzywniak/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Warzywniak.MvcApplication" Language="C#" %>
